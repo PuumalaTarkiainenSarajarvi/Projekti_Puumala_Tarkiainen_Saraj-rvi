2a14fde7b62e67b5db4b7746a9839b55c49049c1
Arttu Tarkiainen <t7taar00@students.oamk.fi>
2018-12-03 14:03:37 +0200

    stylesheet ja muotoilumuutoksia

--- web/application/views/pages/info.php
+++ web/application/views/pages/info.php
@@ -1,9 +1,9 @@
 
     <nav class="infoNav">
         <ul class="infoList">
-            <li class="infoItem"><a href="<?php echo site_url('infomotion') ?>" class="infoLink">Liikkeen seuranta</a></li>
-            <li class="infoItem"><a href="<?php echo site_url('infodoors') ?>" class="infoLink">Ovien seuranta</a></li>
-            <li class="infoItem"><a href="<?php echo site_url('infotemps') ?>" class="infoLink">Tilan lämpötila</a></li>
-            <li class="infoItem"><a href="<?php echo site_url('infopics') ?>" class="infoLink">Kuvat</a></li>
+            <li class="infoItem"><a href="<?php echo site_url('infomotion') ?>" class="infoLink"><i class= "fa fa-arrows"></i> Liikkeen seuranta</a></li>
+            <li class="infoItem"><a href="<?php echo site_url('infodoors') ?>" class="infoLink"><i class= "fa fa-unlock"></i> Ovien seuranta</a></li>
+            <li class="infoItem"><a href="<?php echo site_url('infotemps') ?>" class="infoLink"><i class= "fa fa-thermometer-half"></i> Tilan lämpötila</a></li>
+            <li class="infoItem"><a href="<?php echo site_url('infopics') ?>" class="infoLink"><i class= "fa fa-image"></i> Kuvat</a></li>
         </ul>
     </nav>
--- web/application/views/pages/settings.php
+++ web/application/views/pages/settings.php
@@ -1,7 +1,7 @@
 <div class ="firstRow">
 <nav class="settingsNav">
         <ul class="settingsList">
-            <li class="settingsItem"><a href="<?php echo site_url('alertsettings') ?>" class="settingsLink">Hälytysasetukset</a></li>
-            <li class="settingsItem"><a href="<?php echo site_url('loginsettings') ?>" class="settingsLink">Kirjautumisasetukset</a></li>
+            <li class="settingsItem"><a href="<?php echo site_url('alertsettings') ?>" class="settingsLink"><i class= "fa fa-bell"></i> Hälytysasetukset</a></li>
+            <li class="settingsItem"><a href="<?php echo site_url('loginsettings') ?>" class="settingsLink"><i class= "fa fa-id-card"></i> Kirjautumisasetukset</a></li>
         </ul>
     </nav>
