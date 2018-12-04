83cd271ac99c5a77d5f6a52a83dfb2bc1d29309c
PuumalaO <oskari.puumala@gmail.com>
2018-12-04 13:59:32 +0200

    loginsettings -> loginSettings, linkit eivät enää ole mustia

--- web/application/views/pages/loginsettings.php
+++ web/application/views/pages/loginsettings.php
@@ -1,7 +1,7 @@
 <div class="content">
     <h3>Sähköpostin vaihto</h3>
     <p>Vaihda sähköposti</p>
-      <form class="" action="<?php echo site_url('loginsettings/user_email');?>" method="post">
+      <form class="" action="<?php echo site_url('loginSettings/user_email');?>" method="post">
         <button type="submit" value="button">Täältä</button>
 
       </form>
@@ -10,7 +10,7 @@
     <div class="content">
         <h3>Käyttäjänimen vaihto</h3>
         <p>Vaihda käyttäjänimi</p>
-          <form class="" action="<?php echo site_url('loginsettings/user_username');?>" method="post">
+          <form class="" action="<?php echo site_url('loginSettings/user_username');?>" method="post">
             <button type="submit" value="button">Täältä</button>
 
           </form>
@@ -19,7 +19,7 @@
         <div class="content">
             <h3>Salasanan vaihto</h3>
             <p>Vaihda salasana</p>
-              <form class="" action="<?php echo site_url('loginsettings/user_password');?>" method="post">
+              <form class="" action="<?php echo site_url('loginSettings/user_password');?>" method="post">
                 <button type="submit" value="button">Täältä</button>
 
               </form>
