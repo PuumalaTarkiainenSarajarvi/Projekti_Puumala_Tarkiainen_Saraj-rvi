faf5ad69d14d906091f09065ae069cbf5d7aba2c
PuumalaO <oskari.puumala@gmail.com>
2018-12-10 11:16:11 +0200

    Update infopics.php

--- web/application/views/pages/infopics.php
+++ web/application/views/pages/infopics.php
@@ -1,10 +1,12 @@
 <div class="images">
+  <h2>Valvontakameran kuvat:</h2>
     <?php
      $limit = 0;
      foreach (array_reverse($pics) as $pic)
                 {
                     if($limit == 10) break;
     ?>
-        <img id="img" src=<?php echo $pic ?>>
+        <img id="img" src=<?php echo "/Projekti_Puumala_Tarkiainen_Saraj-rvi/web/$pic"?>>
+        <em><?php echo $pic ?></em>
                 <?php } ?>
 </div>
