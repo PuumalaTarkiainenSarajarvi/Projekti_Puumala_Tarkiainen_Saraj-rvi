17b259c0a19dd754882ab4173b661f03ae1482fb
Pastamamba <pastamamba@gmail.com>
2018-12-11 13:44:30 +0200

    Update etusivu.php

--- web/application/views/pages/etusivu.php
+++ web/application/views/pages/etusivu.php
@@ -1,12 +1,12 @@
 <div class="firstRow">
     <h2>Hei, <?php echo $_SESSION['user']?>!</h2>
-    <h3>Hälytykset ovat tällä hetkellä <?php if($verifyOn && !$manualOn) echo "automaattitilassa"?> 
+    <h3>Hälytykset ovat tällä hetkellä <?php if($verifyOn && !$manualOn) echo "automaattitilassa"?>
     <?php if($manualOn) echo "manuaaliitilassa"?>
     <?php if($manualOn == false && $verifyOn == false) echo "pois käytöstä"?></h3>
-    <h3>Automaattinen hälytys on asetettu välille 
+    <h3>Automaattinen hälytys on asetettu välille
     <?php if(isset($row)) {echo substr($row['dt'],0,-3);?> - <?php echo substr($row['dt2'],0,-3);}?></h3>
     <h3>Viimeisin hälytys tapahtunut <?php echo $recentEvent ?></h3>
-    
+
 </div>
 <h3 class="temp">Lämpötila: <?php echo $temp ?>°C</h3>
 
@@ -14,7 +14,6 @@
 
 function autoUpdate()
 {
-    $('.firstRow').load("<?php echo base_url().'/index.php/ajax/indexdata' ?>");
     $('.firstRow').load("<?php echo base_url().'index.php/ajax/indexdata' ?>");
 }
 
